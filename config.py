f8ba430e31cf239a7b3dc2ce5fe2794ef3f02ae7
bahhbash <bahhbash@gmail.com>
2026-01-24 04:13:26 +0500

    Clear

--- config.py
+++ config.py
@@ -12,52 +12,17 @@ from EnG1nE.Language.Lang import*
 mod = "mod4" #win
 terminal = guess_terminal()
 
-QwertyKeycode = { #xev
-    "ц": "w",
-    "q": "q",
-    "a": "a",
-    "s": "s",
-    "d": "d",
-    "e": "e",
-    "r": "r",
-    "f": "f",
-    "c": "c",
-    "v": "v",
-    "x": "x",
-    "z": "z",
-    "arrow_left": "Left",
-    "arrow_right": "Right",
-    "arrow_up": "Up",
-    "arrow_down": "Down"
-}
-
-def KC(x: str) -> str:
-    try:
-        return QwertyKeycode[x]
-    except:
-        return x
-
 keys = [
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], KC("w"), lazy.window.kill(), desc="Kill focused window"),
-    Key(
-        [mod],
-        "f",
-        lazy.window.toggle_fullscreen(),
-        desc="Toggle fullscreen on the focused window",
-    ),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([], "f11", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
     Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod, "control"], KC("r"), lazy.reload_config(), desc="Reload the config"),
-    Key([mod], KC("r"), lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     Key([mod, "control"], "right", lazy.screen.next_group(), desc="Swap screen"),
     Key([mod, "control"], "left", lazy.screen.prev_group(), desc="Swap screen"),
 ]
 
-# Add key bindings to switch VTs in Wayland.
-# We can't check qtile.core.name in default config as it is loaded before qtile is started
-# We therefore defer the check until the key binding is run by using .when(func=...)
 for vt in range(1, 8):
     keys.append(
         Key(
@@ -68,30 +33,22 @@ for vt in range(1, 8):
         )
     )
 
-
 groups = [Group(i) for i in "123456789"]
-
 for i in groups:
     keys.extend(
         [
-            # mod + group number = switch to group
             Key(
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
                 desc=f"Switch to group {i.name}",
             ),
-            # mod + shift + group number = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
                 desc=f"Switch to & move focused window to group {i.name}",
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod + shift + group number = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
@@ -132,10 +89,7 @@ screens = [
         background="#001320",
         wallpaper="~/Pictures/D.png",
         wallpaper_mode="fill",
-        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-        # By default we handle these events delayed to already improve performance, however your system might still be struggling
-        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
+        x11_drag_polling_rate = 60,
     ),
 ]
 
@@ -147,14 +101,13 @@ mouse = [
 ]
 
 dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
+dgroups_app_rules = []
 follow_mouse_focus = True
 bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
@@ -180,16 +133,7 @@ wl_input_rules = None
 wl_xcursor_theme = None
 wl_xcursor_size = 24
 
-idle_inhibitors = []  # type: list
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
+idle_inhibitors = []
 wmname = "LG3D"
 
 LangInit(keys)
